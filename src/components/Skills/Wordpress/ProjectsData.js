9cff25c372b1a24efc4a984625240955ee4890ef
Rachel Tomi <folorunshotosin334@gmail.com>
2025-08-18 10:42:34 +0100

    updated projects

--- src/components/Skills/Wordpress/ProjectsData.js
+++ src/components/Skills/Wordpress/ProjectsData.js
@@ -1,9 +1,38 @@
 import project3 from "../../../Assets/gojot.png";
 import project2 from "../../../Assets/racheltomiblog.png";
-import project4 from "../../../Assets/hiretalent.png";
+import project4 from "../../../Assets/hiretalentnew.png";
 import project1 from "../../../Assets/ndz.png";
+import project5 from "../../../Assets/allpaws.png";
+import project6 from "../../../Assets/vortex.png";
 
 export const ProjectsData = [
+  {
+    id: "3",
+    title: "Hiretalent Africa ",
+    img: project4,
+    info: "Hiretalent aims to streamline the hiring process, connecting companies with talent – focusing on African professionals.",
+    stacks: ["Wordpress(Elementor)", "Woocommerce", "Figma"],
+    url: "https://hiretalent.africa/",
+    github: "",
+  },
+  {
+    id: "6",
+    title: "Vortex Events",
+    img: project6,
+    info: "Vortex Events creates engaging virtual team experiences like games, workshops, and immersive challenges to boost remote team culture, performance,and fun.",
+    stacks: ["Wordpress", "Figma"],
+    url: "https://allpawsretreat.com/daycare-lp/",
+    github: "",
+  },
+  {
+    id: "5",
+    title: "All Paws Retreat",
+    img: project5,
+    info: "All Paws Retreat is a dog daycare that provides personalized, enrichment-driven care with live camera access and daily photo updates to keep your pup happy, safe, and mentally stimulated.",
+    stacks: ["Wordpress", "Figma"],
+    url: "https://allpawsretreat.com/daycare-lp/",
+    github: "",
+  },
   {
     id: "4",
     title: "NDZ Academy ",
@@ -11,15 +40,6 @@ export const ProjectsData = [
     info: "An online education platform that equips individuals with practical skills to build online businesses or products, aiming to make them job-ready in 8 weeks or less. ",
     stacks: ["Wordpress(Elementor)", "Woocommerce", "Figma"],
     url: "https://ndzacademy.com",
-    github: "",
-  },
-  {
-    id: "1",
-    title: "Rachel Tomi Blog ",
-    img: project2,
-    info: "Rachel Tomi’s blog focuses on career development, personal growth, and remote work strategies.",
-    stacks: ["Wordpress"],
-    url: "https://racheltomi.com",
     github: "",
   },
   {
@@ -32,12 +52,12 @@ export const ProjectsData = [
     github: "",
   },
   {
-    id: "3",
-    title: "Hiretalent Africa ",
-    img: project4,
-    info: "Hiretalent aims to streamline the hiring process, connecting companies with talent – focusing on African professionals.",
-    stacks: ["Wordpress(Elementor)", "Woocommerce", "Figma"],
-    url: "https://wordpress-1389679-5157116.cloudwaysapps.com/",
+    id: "1",
+    title: "Rachel Tomi Blog ",
+    img: project2,
+    info: "Rachel Tomi’s blog focuses on career development, personal growth, and remote work strategies.",
+    stacks: ["Wordpress"],
+    url: "https://racheltomi.com",
     github: "",
   },
 ];
